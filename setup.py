62df9d3f98357ed172adda46781a3e55b63e29cc
SiniKraft <65973766+SiniKraft@users.noreply.github.com>
2021-02-15 21:59:36 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+build_exe_options = {"packages": ["os", "keyboard", "tkinter", "tkinter.ttk", "time", "threading", "win10toast", "pyperclip", "urllib.request", "json", "sys", "webbrowser"]}
+#include_files = ["my_first_file.png"]
+# dans build_exe_options, { 'include_files': include_files }
+setup(
+    name = "Discord Spammer",
+    description = "Discord Spammer by SiniKraft",
+    version = "0.1",
+    description = "Discord Spammer",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("DiscordSpammer.py", base=base, copyright="© SiniKraft 2020-2021", icon="DiscordSpammer.ico", shortcutName="Discord Spammer", shortcutDir="DesktopFolder")],
+)
